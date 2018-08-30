6c6939037f4c434815488c314ff7777f4310bc24
Stephen Kelly <stevekm@users.noreply.github.com>
2018-08-30 14:21:22 -0400

    make report.py work in stand-alone mode

--- Makefile
+++ Makefile
@@ -80,7 +80,7 @@ test:
 # run a demo for example report output
 demo:
 	# app/IR.py
-	app/report.py
+	app/report.py example-data/Seraseq-DNA_RNA-07252018_v1_79026a9c-e0ff-4a32-9686-ead82c35f793-2018-08-21-15-00-11200.tsv
 
 # enter debug console
 # t.records[0].interpretations[0]
--- app/report.py
+++ app/report.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Module for creating reports from Ion Reporter exported .tsv file
 """
 import os
 from jinja2 import FileSystemLoader, Environment, select_autoescape
 import ir
 import pmkb
+import argparse
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 loader = FileSystemLoader(template_dir)
@@ -15,9 +17,24 @@ environment = Environment(
     )
 template = environment.get_template('report.html')
 
-def make_report(input, output):
+def make_report(input, output = None):
     """
+    Makes an HTML report out of an input file.
+
+    Parameters
+    ----------
+    input: str
+        path to input .tsv file
+    output: str
+        path to output .html to create
+
+    Notes
+    -----
+    Output file will be overwritten if it already exists.
     """
+    if not output:
+        output = os.path.splitext(input)[0] + ".html"
+
     # initialize objects for parsing table and database
     IRtable = ir.IRTable(source = input)
     db = pmkb.PMKB()
@@ -30,22 +47,25 @@ def make_report(input, output):
     with open(output, "w") as f:
         f.write(parsed)
 
+def main(**kwargs):
+    """
+    Main control function for the script
+    """
+    input = kwargs.pop('input')[0]
+    output = kwargs.pop('output', None)
+    make_report(input = input, output = output)
 
+def parse():
+    """
+    Parses script args
+    """
+    parser = argparse.ArgumentParser(description='Creates a report from an input .tsv file')
+    parser.add_argument('input', nargs=1, help="Ion Reporter .tsv table input file")
+    parser.add_argument("-o", "--output", default = None, dest = 'output', help="Output file path")
 
+    args = parser.parse_args()
+
+    main(**vars(args))
 
 if __name__ == '__main__':
-    import ir
-    import pmkb
-
-    output_html = "output.html"
-    IRtable = ir.demo()
-    PMKB, _ = pmkb.demo()
-    IRtable.lookup_all_interpretations(db = PMKB)
-    # IRtable.records[1].interpretations
-
-    parsed = template.render(IRtable = IRtable)
-
-    with open(output_html, "w") as f:
-        f.write(parsed)
-    # from dev import debugger
-    # debugger(globals().copy())
+    parse()
