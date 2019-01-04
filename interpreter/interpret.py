653d99ec543ae1530eead9512472f082c4425363
Stephen K <steve.at.nyu1@gmail.com>
2019-01-04 17:41:53 -0500

    update interpretation query method

--- interpreter/interpret.py
+++ interpreter/interpret.py
@@ -6,6 +6,7 @@ Module for handling the interpretation of IonReporter .tsv results against datab
 import os
 import sys
 import django
+from collections import defaultdict
 
 # import app from top level directory
 parentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -16,16 +17,7 @@ from interpreter.models import PMKBVariant, PMKBInterpretation
 from interpreter.ir import IRTable
 sys.path.pop(0)
 
-class PMKBResults(object):
-    """
-    Object for aggregating database query results for PMKB variants and their interpretations
-    """
-    def __init__(self):
-        self.variants = []
-        self.interpretations = []
-
 def query_variant(model, **params):
-    # print(params)
     variants = model.objects.filter(**params)
     return(variants)
 
@@ -33,39 +25,31 @@ def query_pmkb(genes, **params):
     tissue_type = params.pop('tissue_type', None)
     tumor_type = params.pop('tumor_type', None)
     variant = params.pop('variant', None)
-    results = PMKBResults()
-    for gene in genes:
-        PMKB_params = {'gene': gene}
-        if tissue_type:
-            PMKB_params['tissue_type'] = tissue_type
-        if tumor_type:
-            PMKB_params['tumor_type'] = tumor_type
-        if variant:
-            PMKB_params['variant'] = variant
-        for variant in query_variant(model = PMKBVariant, **PMKB_params):
-            results.variants.append(variant)
+    variant_query = PMKBVariant.objects.filter(gene__in = genes)
+    if tissue_type:
+        variant_query = variant_query.filter(tissue_type = tissue_type)
+    if tumor_type:
+        variant_query = variant_query.filter(tumor_type = tumor_type)
+    if variant:
+        variant_query = variant_query.filter(variant = variant)
+
+    interpretations = defaultdict(set)
+    for variant_result in variant_query:
+        interpretations[variant_result.interpretation].add(variant_result)
+    results = []
+    for key, value in interpretations.items():
+        d = {'interpretation': key, 'variants': list(value)}
+        results.append(d)
     return(results)
 
 def demo():
-    tissueType = "Skin"
-    tumorType = "Melanoma"
+    tumor_type = "Skin"
+    tissue_type = "Melanoma"
     ir_tsv = sys.argv[1] # "example-data/SeraSeq.tsv"
     ir_table = IRTable(source = ir_tsv)
-    # print(ir_table.records)
     for record in ir_table.records:
-        print(record.genes)
-        for gene in record.genes:
-            PMKB_params = {
-            'gene': gene,
-            'tumor_type': tumorType,
-            'tissue_type': tissueType
-            }
-            PMKB_variants = query_variant(model = PMKBVariant, **PMKB_params)
-            # record.interpretations
-            for variant in PMKB_variants:
-                # interpretation = PMKBInterpretation.objects.get(interpretation = variant.interpretation)
-                print(variant.interpretation.interpretation)
-        # break
+        results = query_pmkb(genes = record.genes, params = {'tissue_type': tissue_type, 'tumor_type': tumor_type})
+        print(results)
 
 if __name__ == '__main__':
     demo()
