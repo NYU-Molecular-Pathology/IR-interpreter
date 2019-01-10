b07e58cfba545d42e613ffbd93634f8b20dd74f5
Stephen Kelly <stevekm@users.noreply.github.com>
2019-01-09 21:43:56 -0500

    start urls, routes, views

---
+++ interpreter/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+app_name = 'interpreter'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('samplesheet/upload', views.samplesheet_upload, name='samplesheet_upload')
+]
